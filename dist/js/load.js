3471740fc1563323278b5faf85f7694ef76758e5
Ariefianto Rizky <ariefiantorizky2907@gmail.com>
2021-10-19 13:20:32 +0700

    update to v1.3.5

---
+++ dist/js/load.js
@@ -0,0 +1,9 @@
+const soal = document.querySelector(".soal");
+soal.style.display = "none";
+
+window.addEventListener("load", function () {
+    const load = document.querySelector(".load");
+    load.style.display = "none";
+
+    soal.style.display = "block";
+})
